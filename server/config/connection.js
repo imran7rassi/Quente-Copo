298a650fb7efab08c5ad39d64c74a616a6d7ba6d
imran-rassi <imran.rassi.2020@gmail.com>
2023-02-16 19:27:57 -0500

    adding the environment

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/buskr', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/trouper', {
 	useNewUrlParser    : true,
 	useUnifiedTopology : true,
 	useCreateIndex     : true,
