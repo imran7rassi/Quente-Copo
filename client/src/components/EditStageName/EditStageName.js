50d372575007a872c99ced8983dcad3cbd4a1fd4
imran-rassi <imran.rassi.2020@gmail.com>
2023-02-18 23:43:46 -0500

    added the components

---
+++ client/src/components/EditStageName/EditStageName.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import { useMutation } from '@apollo/react-hooks';
+
+import { useToggle, useInputState } from '../../hooks';
+import { UPDATE_CREATOR_STAGE_NAME } from '../../utils/mutations';
+
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
+
+import './EditStageName.css';
+
+const EditStageName = ({ curStageName }) => {
+	// MODAL TOGGLE
+	const [ show, toggleShow ] = useToggle(false);
+
+	const [ stageName, setStageName ] = useInputState(curStageName);
+
+	// DESTRUCTURE MUTATION FUNCTION FOR USE ON FORM SUBMIT
+	const [ updateCreatorStageName ] = useMutation(UPDATE_CREATOR_STAGE_NAME);
+
+	const handleFormSubmit = async (e) => {
+		// close modal
+		handleClose();
+		e.preventDefault();
+
+		try {
+			await updateCreatorStageName({
+				variables : {
+					stageName
+				}
+			});
+		} catch (err) {
+			console.error(err);
+		}
+	};
+
+	// MODAL DISPLAY
+	const handleClose = () => toggleShow();
+	const handleShow = () => toggleShow();
+
+	return (
+		<>
+			<Button
+				className="EditStageName-Btn p-2 w-50 btn-sm bskr-btn-purple"
+				variant="primary"
+				onClick={handleShow}
+			>
+				edit stage name
+			</Button>
+
+			<Modal
+				className="EditStageName "
+				centered
+				show={show}
+				onHide={handleClose}
+				animation={false}
+			>
+				<Modal.Header closeButton>
+					<Modal.Title>edit your stage name</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>
+					<Form onSubmit={handleFormSubmit}>
+						<FormControl
+							name="loc"
+							aria-label="With input"
+							onChange={setStageName}
+							value={stageName || ''}
+							placeholder={curStageName}
+						/>
+						<Button
+							className="mt-3"
+							type="submit"
+							variant="primary btn-sm bskr-btn-purple"
+						>
+							save
+						</Button>
+					</Form>
+				</Modal.Body>
+			</Modal>
+		</>
+	);
+};
+
+export default EditStageName;
---
+++ client/src/components/EditVibes/EditVibes.js
@@ -0,0 +1,126 @@
+import React, { useState, useEffect } from 'react';
+import { useMutation, useQuery } from '@apollo/react-hooks';
+
+import { useToggle } from '../../hooks'
+import { QUERY_VIBES } from '../../utils/queries';
+import { UPDATE_CREATOR_VIBES } from '../../utils/mutations';
+
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
+import './EditVibes.css';
+
+// destructure vibe objects of current creator from props
+const EditVibes = ({ curVibes }) => {
+  // MODAL TOGGLE
+  const [ show, toggleShow ] = useToggle(false);
+
+	const [ allVibes, setAllVibes ] = useState([]);
+
+	// map an array of the ids of current vibes to set defaultChecked of checkboxes for matched ids
+	const curVibesIds = curVibes?.map(vibe => vibe._id);
+
+  // query to get all vibes
+	const { loading, data } = useQuery(QUERY_VIBES);
+
+  // initialize mutation function to update creator vibes
+	const [ updateCreatorVibes ] = useMutation(UPDATE_CREATOR_VIBES);
+
+  // update component state when data arrives from db query
+	useEffect(
+		() => {
+			if (data) {
+				setAllVibes(data);
+			}
+		},
+		[ data, loading ]
+	);
+
+	// MODAL FUNCTIONALITY
+	const handleClose = () => toggleShow();
+	const handleShow = () => toggleShow();
+
+  // call mutation function with array of updated vibe id's
+	const handleFormSubmit = async (e) => {
+    e.preventDefault();
+    // close modal
+		handleClose();
+		// array-like iterable (RadioNodeList) of all checkbox els
+		const vibes = e.target.elements['updatedVibes'];
+		// initialize array of updated vibes
+		let updatedVibes = [];
+		// iterate over RadioNodeList and push the value (vibe name) of each checked box into the array
+		for (let i = 0; i < vibes.length; i++) {
+			vibes[i].checked && updatedVibes.push(vibes[i].value);
+		}
+
+    try {
+      const mutationResponse = await updateCreatorVibes({
+        variables : {
+          vibes : updatedVibes
+        }
+			});
+      console.log('mutationResponse', mutationResponse);
+
+			// window.location.reload()
+		} catch (err) {
+			console.error(err);
+		}    
+	};
+
+	return (
+		<>
+			{allVibes ? (
+				<>
+					<Button
+						className="w-50 btn-sm bskr-btn-purple"
+						variant="primary"
+						onClick={handleShow}
+					>
+						edit your vibes
+					</Button>
+
+					<Modal
+						className="EditVibesModal"
+						centered
+						show={show}
+						onHide={handleClose}
+						animation={false}
+					>
+						<Modal.Header closeButton>
+							<Modal.Title>edit your vibes</Modal.Title>
+						</Modal.Header>
+						<Modal.Body>
+							<Form onSubmit={handleFormSubmit}>
+								<Form.Group>
+                  {/* All is included until refactor of All filter on CreatrGrid */}
+									{allVibes.vibes?.map((vibe) => (
+									<div key={vibe._id} className="mb-3">
+										<Form.Check
+                      // type="checkbox"
+                      disabled={vibe.name === 'All'}
+											name="updatedVibes"
+											label={vibe.name}
+											value={vibe._id}
+											defaultChecked={curVibesIds?.includes(vibe._id)}
+										/>
+									</div>
+									))}
+								</Form.Group>
+								<Button
+									variant="primary btn-sm bskr-btn-purple"
+									type="submit"
+								>
+									save
+								</Button>
+							</Form>
+						</Modal.Body>
+					</Modal>
+				</>
+			) : null}
+		</>
+	);
+};
+
+export default EditVibes;
