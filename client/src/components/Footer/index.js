fd40fb761b72752f7229b349a7437096523bf6af
imran-rassi <imran.rassi.2020@gmail.com>
2023-02-23 17:01:07 -0500

    changed footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -8,9 +8,9 @@ const Footer = () => {
         <a href="#" className="px-2">Terms of Use</a>
       </div>
       <div className="fa-lg text-light list-inline-item">
-        <a href="https://github.com/Project-3-UT-Bootcamp/proxy"><i className="fab fa-github px-2"></i></a>
-        <a href="#"><i className="fab fa-linkedin px-2"></i></a>
-        <a href="#"><i className="fab fa-facebook px-2"></i></a>
+        <a href="https://github.com/imran7rassi"><i className="fab fa-github px-2"></i></a>
+        <a href="https://www.linkedin.com/in/imran-rasi-a01a98259/"><i className="fab fa-linkedin px-2"></i></a>
+        <a href="https://www.facebook.com/imran.khan.rassi"><i className="fab fa-facebook px-2"></i></a>
       </div>
       
 
