7992149a05327e1f8106477f29db4d813ee4fb57
imran-rassi <imran.rassi.2020@gmail.com>
2023-02-23 19:32:37 -0500

    adding the readme

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -8,7 +8,7 @@ const Footer = () => {
         <a href="#" className="px-2">Terms of Use</a>
       </div>
       <div className="fa-lg text-light list-inline-item">
-        <a href="https://github.com/Project-3-UT-Bootcamp/proxy"><i className="fab fa-github px-2"></i></a>
+        <a href="https://github.com/imran7rassi"><i className="fab fa-github px-2"></i></a>
         <a href="#"><i className="fab fa-linkedin px-2"></i></a>
         <a href="#"><i className="fab fa-facebook px-2"></i></a>
       </div>
